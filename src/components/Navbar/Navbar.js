53e7ce20539e21621c62843ca8ab4a99af25c06a
Trung Cao <caot1@nku.edu>
2022-05-22 13:14:24 -0400

    Conditional show user content

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,35 @@
 import { Link } from 'react-router-dom'
 import styles from './Navbar.module.css'
 import { useLogout } from '../../hooks/useLogout'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 export default function Navbar() {
     const { logout } = useLogout()
+    const { user }= useAuthContext()
 
     return (
         <nav className={styles.navbar}>
             <ul>
                 <li className={styles.title}>myMoney</li>
-                <li><Link to='/login'>Login</Link></li>
-                <li><Link to='/signup'>Signup</Link></li>
-                <li>
-                    <button className='btn' onClick={logout}>Logout</button>
-                </li>
+                
+                {/* User is currently not logged in */}
+                {!user && (
+                    <>
+                        <li><Link to='/login'>Login</Link></li>
+                        <li><Link to='/signup'>Signup</Link></li>
+                    </>
+                )}
+
+                {/* User is currently logged in */}
+                {user && (
+                    <>
+                        <li>Hello, {user.displayName}</li>
+                        <li>
+                            <button className='btn' onClick={logout}>Logout</button>
+                        </li>
+                    </>
+                )}
+                
             </ul>
         </nav>
     )
